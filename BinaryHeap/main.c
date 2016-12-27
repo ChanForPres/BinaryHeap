5031ed01facecf7a81f3e67b89b07fb97d12fbe5
Marcus Chan <marcus@marcuss-mbp>
2016-12-27 15:18:33 -0800

    Initial Commit

---
+++ BinaryHeap/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BinaryHeap
+//
+//  Created by Marcus Chan on 12/27/16.
+//  Copyright © 2016 Marcus Chan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
